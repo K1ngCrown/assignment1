48ef3192ff4751a7f55277edb5dede15e8a21a9e
K1ngCrown <43577015+K1ngCrown@users.noreply.github.com>
2022-09-01 15:33:53 +1000

    Uploading the IGB283 Tranform file

---
+++ IGB283Transform.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IGB283Transform 
+{
+    // This class is used to detail the data for the shape tranformations
+
+
+    public static Matrix3x3 Scale()
+    {
+        Matrix3x3 matrix = new Matrix3x3();
+        return matrix;
+    }
+
+    public static Matrix3x3 Transalte()
+    {
+        Matrix3x3 matrix = new Matrix3x3();
+        return matrix;
+    }
+    public static Matrix3x3 Rotate(float angle)
+    {
+        Matrix3x3 matrix = new Matrix3x3();
+
+        matrix.SetRow(0, new Vector3(Mathf.Cos(angle), -Mathf.Sin(angle), 0));
+        matrix.SetRow(1, new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0));
+        matrix.SetRow(2, new Vector3(0.0f, 0.0f, 1.0f));
+
+        return matrix;
+    }
+
+
+
+
+
+
+}
