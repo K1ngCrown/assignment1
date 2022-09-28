13fe1537e6177b3cd8f39d1c113fc309ee8f50d2
104ironmadam <93643617+104ironmadam@users.noreply.github.com>
2022-09-28 16:32:56 +1000

    Update SecondSpaceShip.cs

--- SecondSpaceShip.cs
+++ SecondSpaceShip.cs
@@ -1,49 +1,36 @@
 using UnityEngine;
 using System.Collections;
 
-public class SecondSpaceShip : MonoBehaviour
+public class SecondSpaceship : MonoBehaviour
 {
-    /// <summary>
-    /// This class is used to detail the spaceship game object
-    /// </summary>
-
     Mesh mesh;
 
     public Material material;
     public float theta = 1f;
-    public float speed = 1f;
+    public float speed = 0f;
 
-    private float shift_up = 3f;
-    public bool goUp;
-
-    private float right_planet_x = 7;
+    private float right_planet_x = 7f;
     private float left_planet_x = -7f;
-
-    
 
     private Vector3 offset;
 
     public float size;
     public float increaseSize = 1.0004f;
     public float decreaseSize = 0.9996f;
-
+   
     // Start is called before the first frame update
     void Start()
     {
-        goUp = true;
-        //size = increaseSize;
-        // Add a MeshFilter and MeshRenderer to the Empty GameObject
+        Vector3 StartingPos = new Vector3(0f, 3f, 0);
+        
         mesh = gameObject.AddComponent<MeshFilter>().mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
-
-
-        // CLear all vertex and index data from the mesh
+       
         mesh.Clear();
 
         // Create a triangles with points
         mesh.vertices = new Vector3[]
         {
-            
             // Point no. (google sheet)
             new Vector3(0f, 0.95f), // 1
             new Vector3(-0.35f, 0.5f), // 2
@@ -69,13 +56,11 @@ public class SecondSpaceShip : MonoBehaviour
             new Vector3(0.3f, -0.7f), // 22
             new Vector3(0.2f, -0.6f), // 23            
             new Vector3(0.0f, -0.95f) // 24
-            
-
         };
 
         // This section sets the colour of the spaceships vertices
         Vector3[] vertices = mesh.vertices;
-
+        
 
         // Set vertex indices
         mesh.triangles = new int[]
@@ -95,13 +80,23 @@ public class SecondSpaceShip : MonoBehaviour
             20, 21, 22, // M             
             19, 22, 23 // N
         };
-
+        
         // Calculate the bounds of the shape
         offset.x = mesh.bounds.size.x / 2;
         offset.y = mesh.bounds.size.y / 2;
 
-       
-        
+        Matrix3x3 TopShipTranslate = IGB283Transform.Translate(StartingPos);
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = TopShipTranslate.MultiplyPoint(vertices[i]);
+        }
+
+        // Write the points back to the mesh
+        mesh.vertices = vertices;
+
+        mesh.RecalculateBounds();
+        offset = mesh.bounds.center;
     }
 
     // Update is called once per frame
@@ -112,7 +107,7 @@ public class SecondSpaceShip : MonoBehaviour
         for (int i = 0; i < vertices.Length; i++)
         {
             // vertices will be orange
-            if (offset.y > 0)
+            if (offset.x > 0)
             {
                 colors[i] = new Color(0.8f, 0.3f, 0.3f, 1.0f);
 
@@ -125,6 +120,7 @@ public class SecondSpaceShip : MonoBehaviour
 
         }
         mesh.colors = colors;
+        
 
         if (offset.x > right_planet_x || offset.x < left_planet_x)
         {
@@ -141,8 +137,7 @@ public class SecondSpaceShip : MonoBehaviour
             size = decreaseSize;
         }
         
-        //Matrices definition
-        Matrix3x3 TranslateUp = IGB283Transform.Translate(offset + new Vector3(0, speed / 150, 0));
+
         Matrix3x3 RotateShip = IGB283Transform.Rotate(theta * Time.deltaTime * 3f);
         Matrix3x3 Translate = IGB283Transform.Translate(offset + new Vector3(speed / 150, 0, 0));
         Matrix3x3 Scale = IGB283Transform.Scale(size, size);
@@ -150,15 +145,10 @@ public class SecondSpaceShip : MonoBehaviour
         Matrix3x3 Transformation = Translate * RotateShip * Scale * Translate_Back;
         Matrix3x3 NoScaleTransformation = Translate * RotateShip * Translate_Back;
 
-
         // Apply the transformation to all the points in the mesh
         for (int i = 0; i < vertices.Length; i++)
         {
-            if (goUp == true && offset.y < shift_up)
-            {
-                vertices[i] = TranslateUp.MultiplyPoint(vertices[i]);
-            }
-            else if (speed != 0)
+            if (speed != 0)
             {
                 vertices[i] = Transformation.MultiplyPoint(vertices[i]);
             }
@@ -166,7 +156,6 @@ public class SecondSpaceShip : MonoBehaviour
             {
                 vertices[i] = NoScaleTransformation.MultiplyPoint(vertices[i]);
             }
-
         }
 
         // Write the points back to the mesh
@@ -175,46 +164,23 @@ public class SecondSpaceShip : MonoBehaviour
         mesh.RecalculateBounds();
         offset = mesh.bounds.center;
 
-
         //Change speed of spaceship
-        if (Input.GetKeyDown("o") && speed >= 1)
+        if (Input.GetKeyDown("w") && speed >= 1) 
         {
             speed += 1f;
         }
-        else if (Input.GetKeyDown("o") && speed == 0)
+        else if (Input.GetKeyDown("s") && speed == 0)
         {
             speed += 1f;
         }
-        else if (Input.GetKeyDown("p") && speed > 1)
+        else if (Input.GetKeyDown("l") && speed > 1) 
         {
             speed -= 1f;
         }
-        else if (Input.GetKeyDown("p") && speed == 1)
+        else if (Input.GetKeyDown("l") && speed == 1)
         {
             speed = 0;
         }
-
-
-
-        /*
-       //Change the speed of shape rotation
-       if ((Input.GetKeyDown("k")) && theta < 7.0f)
-       {
-           theta += 0.1f;
-       }
-       else if ((Input.GetKeyDown("l")) && theta > 0f)
-       {
-           theta -= 0.1f;
-           if (theta < 0.1f)
-           {
-               theta = 0f;
-           }
-       }
-       */
-
-
-
-
     }
 
 }
